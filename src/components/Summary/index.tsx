5c5c3f22f1f55c252d694aa3b5353c9f43d17d7c
Bruna <brunaporato@gmail.com>
2023-11-15 09:55:52 -0300

    add hook useSummary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,29 +1,10 @@
-import { useContext } from "react";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { TransactionContext } from "../../contexts/TransactionsContext";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionContext);
-
-  // reduzir o array de transactions a um objeto { income: number, outcome: number, total: number }
-  // acc = objeto inicial
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if(transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-      return acc
-    },
-  { income: 0,
-    outcome: 0,
-    total: 0
-  })
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { TransactionContext } from "../contexts/TransactionsContext";
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionContext);
+
+  // reduzir o array de transactions a um objeto { income: number, outcome: number, total: number }
+  // acc = objeto inicial
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if(transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+      return acc
+    },
+    { income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
+  return summary
+}
