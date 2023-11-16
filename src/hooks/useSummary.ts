988f37ea5cc3f1ba05e449086a442af7fa413be3
Bruna <brunaporato@gmail.com>
2023-11-16 16:48:07 -0300

    add hooks usememo & usecallback

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { TransactionContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
@@ -8,19 +9,21 @@ export function useSummary() {
 
   // reduzir o array de transactions a um objeto { income: number, outcome: number, total: number }
   // acc = objeto inicial
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [transactions])
 
   return summary
 }
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -6,6 +6,23 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionContext } from '../../../../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
+/**
+ * Pq um componente renderiza?
+ * - Hooks changed (mudou estado, contexto, reducer);
+ * - Props changed;
+ * - Parent rerendered;
+ *
+ * Qual o fluxo de renderização?
+ * 1. O react recria o html da interface daquele componente;
+ * 2. Compara a versão do html recriada com a versão anterior;
+ * 3. SE mudou alguma coisa, ele reescreve o html na tela;
+ *
+ * Memo:
+ * 0. Hooks changed, Props changed (shallow compare);
+ * 0.1. Comparar com a versão anterior dos hooks e props;
+ * 0.2. SE mudou algo, ele vai permitir a nova renderização;
+ */
+
 const searchFormSchema = z.object({
   query: z.string(),
 })
